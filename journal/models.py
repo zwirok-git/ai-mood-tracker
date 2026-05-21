1409546cf713abd56f5843138de2a41753bf7574
zwirok-git <oleksijprokopenko@gmail.com>
2026-05-21 08:37:32 +0300

    feat: implement AI status system

--- journal/models.py
+++ journal/models.py
@@ -1,3 +1,21 @@
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    class Meta:
+        ordering = ["-last_login",]
+
+
+class AIStatus(models.TextChoices):
+    PENDING = "PENDING", "Pending"
+    PROCESSING = "PROCESSING", "Processing"
+    COMPLETE = "COMPLETE", "Complete"
+    FAILED = "FAILED", "Failed"
+
+
+
+
+
+

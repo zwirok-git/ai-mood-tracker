fc74c04c26c12c1cadab13b87374d11b9c8f8eb5
zwirok-git <oleksijprokopenko@gmail.com>
2026-05-21 18:54:51 +0300

    feat: add routing for home and journal pages

--- ai_mood_tracker/urls.py
+++ ai_mood_tracker/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from journal.views import IndexView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", IndexView.as_view(), name="home"),
+    path("journal/", include("journal.urls")),
 ]

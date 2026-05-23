a44f103f3ac47cdb066b07414a4fe5bce6a08697
zwirok-git <oleksijprokopenko@gmail.com>
2026-05-23 20:52:35 +0300

    feat: add routes for user profile and user profile update

--- journal/urls.py
+++ journal/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from journal.views import UserLoginView
+from journal.views import UserLoginView, UserDetailView, UserUpdateView
 
 urlpatterns = [
-    path("login/", UserLoginView.as_view(), name="login")
+    path("login/", UserLoginView.as_view(), name="login"),
+    path("profile/<int:pk>/", UserDetailView.as_view(), name="profile"),
+    path("profile/<int:pk>/edit/", UserUpdateView.as_view(), name="profile-edit"),
 ]

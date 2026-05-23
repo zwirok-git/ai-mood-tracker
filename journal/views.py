9b69f0c33ad987264f8844a59a0a5ce8b9d84817
zwirok-git <oleksijprokopenko@gmail.com>
2026-05-23 18:40:12 +0300

    feat: implement user login view

--- journal/views.py
+++ journal/views.py
@@ -1,6 +1,15 @@
+from django.contrib.auth.views import LoginView
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views import generic
+
+from journal.forms import UserLoginForm
 
 
 class IndexView(generic.TemplateView):
     template_name = "landing.html"
+
+
+class UserLoginView(LoginView):
+    form_class = UserLoginForm
+    template_name = "registration/login.html"

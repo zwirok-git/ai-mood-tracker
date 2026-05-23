e508824299827ce9e1b97b0d40361c01950df6e7
zwirok-git <oleksijprokopenko@gmail.com>
2026-05-23 18:43:02 +0300

    chore: update redirect urls in settings

--- ai_mood_tracker/settings.py
+++ ai_mood_tracker/settings.py
@@ -136,7 +136,8 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
+LOGIN_URL = "/login/"
 LOGIN_REDIRECT_URL = "/"
-LOGOUT_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/login/"
 
 AUTH_USER_MODEL = "journal.User"

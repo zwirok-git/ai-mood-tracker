e184edaa985fc626384575930486d00b7da6ee19
zwirok-git <oleksijprokopenko@gmail.com>
2026-05-23 22:27:08 +0300

    refactor: reorganize project URL configuration

--- ai_mood_tracker/settings.py
+++ ai_mood_tracker/settings.py
@@ -137,7 +137,7 @@ STATICFILES_DIRS = [
 ]
 
 LOGIN_URL = "/login/"
-LOGIN_REDIRECT_URL = "/"
-LOGOUT_REDIRECT_URL = "/login/"
+LOGIN_REDIRECT_URL = "home"
+LOGOUT_REDIRECT_URL = "login"
 
 AUTH_USER_MODEL = "journal.User"
--- journal/urls.py
+++ journal/urls.py
@@ -1,9 +1,13 @@
+from django.contrib.auth.views import LogoutView
 from django.urls import path
 
-from journal.views import UserLoginView, UserDetailView, UserUpdateView
+from journal.views import UserLoginView, UserDetailView, UserUpdateView, UserCreateView, IndexView
 
 urlpatterns = [
     path("login/", UserLoginView.as_view(), name="login"),
     path("profile/<int:pk>/", UserDetailView.as_view(), name="profile"),
     path("profile/<int:pk>/edit/", UserUpdateView.as_view(), name="profile-edit"),
+    path("logout/", LogoutView.as_view(), name="logout"),
+    path("register/", UserCreateView.as_view(), name="register"),
+    path("", IndexView.as_view(), name="home")
 ]

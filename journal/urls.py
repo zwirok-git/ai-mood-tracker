8816ee67303de90086d3f3746e198081383b394a
zwirok-git <oleksijprokopenko@gmail.com>
2026-05-24 12:03:50 +0300

    feat: add route to delete page confirmation

--- journal/urls.py
+++ journal/urls.py
@@ -1,7 +1,14 @@
 from django.contrib.auth.views import LogoutView
 from django.urls import path
 
-from journal.views import UserLoginView, UserDetailView, UserUpdateView, UserCreateView, IndexView
+from journal.views import (
+    UserLoginView,
+    UserDetailView,
+    UserUpdateView,
+    UserCreateView,
+    IndexView,
+    UserDeleteView,
+)
 
 urlpatterns = [
     path("login/", UserLoginView.as_view(), name="login"),
@@ -9,5 +16,8 @@ urlpatterns = [
     path("profile/<int:pk>/edit/", UserUpdateView.as_view(), name="profile-edit"),
     path("logout/", LogoutView.as_view(), name="logout"),
     path("register/", UserCreateView.as_view(), name="register"),
-    path("", IndexView.as_view(), name="home")
+    path("", IndexView.as_view(), name="home"),
+    path("profile/<int:pk>/delete/", UserDeleteView.as_view(), name="profile-delete"),
+
+    # HTMX-urls
 ]

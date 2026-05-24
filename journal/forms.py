fd9395a1c936ac848b4b38d974a814bd00b64ad5
zwirok-git <oleksijprokopenko@gmail.com>
2026-05-24 21:23:20 +0300

    feat: add journal entry form

--- journal/forms.py
+++ journal/forms.py
@@ -1,6 +1,19 @@
 from django import forms
 from django.contrib.auth.forms import AuthenticationForm
+from journal.models import JournalEntry
 
 
 class UserLoginForm (AuthenticationForm):
     pass
+
+
+class JournalEntryForm(forms.ModelForm):
+
+    class Meta:
+        model = JournalEntry
+
+        fields = (
+            "title",
+            "content",
+            "mood_score",
+        )

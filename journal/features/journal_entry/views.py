20c10cd689d321ed4fd53f53ace0a5555b7d5cc6
zwirok-git <oleksijprokopenko@gmail.com>
2026-05-24 21:23:08 +0300

    feat: add journal entry create view

--- journal/features/journal_entry/views.py
+++ journal/features/journal_entry/views.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
 from django.views import generic
 
+from journal.forms import JournalEntryForm
 from journal.models import JournalEntry
 
 
@@ -15,7 +17,16 @@ class JournalEntryListView(LoginRequiredMixin, generic.ListView):
 
 class JournalEntryCreateView(LoginRequiredMixin, generic.CreateView):
     model = JournalEntry
+    form_class = JournalEntryForm
     template_name = "journal/journal_entry/journal_entry_create_form.html"
+    success_url = reverse_lazy("journal:journal-entry-list")
+
+    def form_valid(self, form,):
+        form.instance.user = self.request.user
+
+        return super().form_valid(form)
+
+
 
 
 class JournalEntryUpdateView(LoginRequiredMixin, generic.UpdateView):

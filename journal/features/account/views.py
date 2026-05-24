cb6132372405755336da6c164a038207b227c0c6
zwirok-git <oleksijprokopenko@gmail.com>
2026-05-24 19:39:14 +0300

    refactor: add template names to views & delete unused import

--- journal/features/account/views.py
+++ journal/features/account/views.py
@@ -1,14 +1,12 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from django.contrib.auth.views import LoginView
 from django.urls import reverse_lazy
 from django.views import generic
-
-from journal.forms import UserLoginForm
 
 
 class UserDetailView(LoginRequiredMixin, UserPassesTestMixin, generic.DetailView):
     model = get_user_model()
+    template_name = "journal/user/user_detail.html"
 
     def test_func(self):
         return  self.request.user == self.get_object()
@@ -21,13 +19,13 @@ class UserUpdateView(LoginRequiredMixin, UserPassesTestMixin, generic.UpdateView
         "first_name",
         "last_name",
         "email",
-
     ]
-    template_name_suffix = "_update_form"
+    template_name = "journal/user/user_update_form.html"
     success_url = "/"
 
     def test_func(self):
         return  self.request.user == self.get_object()
+
 
 class UserCreateView(generic.CreateView):
     pass
@@ -35,6 +33,7 @@ class UserCreateView(generic.CreateView):
 
 class UserDeleteView(LoginRequiredMixin, UserPassesTestMixin, generic.DeleteView):
     model = get_user_model()
+    template_name = "journal/user/user_confirm_delete.html"
     success_url = reverse_lazy("home")
 
     def test_func(self):

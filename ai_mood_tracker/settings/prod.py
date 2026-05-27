c19209cc4dfa8341a3fc806b59adfc6d326f7dd5
zwirok-git <oleksijprokopenko@gmail.com>
2026-05-27 20:15:41 +0300

    chore: add .env sample

--- ai_mood_tracker/settings/prod.py
+++ ai_mood_tracker/settings/prod.py
@@ -5,42 +5,38 @@ from .base import *
 
 DEBUG = False
 
+SECURE_SSL_REDIRECT = True
+
+SESSION_COOKIE_SECURE = True
+
+CSRF_COOKIE_SECURE = True
+
+SECURE_HSTS_SECONDS = 31536000
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
+
+SECRET_KEY = os.environ["SECRET_KEY"]
+
+RENDER_EXTERNAL_HOSTNAME = os.environ.get("RENDER_EXTERNAL_HOSTNAME")
+if RENDER_EXTERNAL_HOSTNAME:
+   ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
+
 
 DATABASES = {
     "default": {
         "ENGINE": (
             "django.db.backends.postgresql"
         ),
-
-        "NAME": os.environ[
-            "PGDATABASE"
-        ],
-
-        "USER": os.environ[
-            "PGUSER"
-        ],
-
-        "PASSWORD": os.environ[
-            "PGPASSWORD"
-        ],
-
-        "HOST": os.environ[
-            "PGHOST"
-        ],
-
-        "PORT": os.environ.get(
-            "PGPORT",
-            5432,
-        ),
-
+        "NAME": os.environ["POSTGRES_DB"],
+        "USER": os.environ["POSTGRES_USER"],
+        "PASSWORD": os.environ["POSTGRES_PASSWORD"],
+        "HOST": os.environ["POSTGRES_HOST"],
+        "PORT": os.environ["POSTGRES_DB_PORT"],
         "OPTIONS": {
             "sslmode": "require",
         },
-
-        "DISABLE_SERVER_SIDE_CURSORS": True,
-
-        "CONN_HEALTH_CHECKS": True,
-    },
+    }
 }
 
 
@@ -57,3 +53,4 @@ ANYMAIL = {
         "RESEND_API_KEY"
     ],
 }
+
